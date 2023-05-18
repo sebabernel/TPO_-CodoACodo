aba0c41e5eee9968b6c8dba233d014c1d7a5697d
MariaVillafañe <kristyv830@gmail.com>
2023-05-18 09:22:48 -0300

    agregando platos.html,la sección del menú

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function showMenu(){
+    document.querySelector('.navigation').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('hide');
+    document.querySelector('.close').classList.toggle('show');
+}
