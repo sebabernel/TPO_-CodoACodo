61d4dac27b8d346665f82afc88a4072acf31557c
marcelo136 <marcelocespedes2011@gmail.com>
2023-05-19 13:24:14 -0300

    actualización nueva

--- main.js
+++ main.js
@@ -4,10 +4,20 @@ function showMenu(){
     document.querySelector('.close').classList.toggle('show');
 }
 
-function mostrarPopUp() {
-    // Mostrar el pop-up
-    document.getElementById("pop-up").style.display = "block";
+document.getElementById('reservationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
   
-    // Evitar que se envíe el formulario de inmediato
-    return false;
-  }
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var fecha = document.getElementById('fecha').value;
+    var comensales = document.getElementById('comensales').value;
+  
+    if (nombre.trim() === '' || apellido.trim() === '' || email.trim() === '' || fecha.trim() === '' || comensales.trim() === '' || isNaN(comensales)) {
+      alert('Por favor, complete todos los campos correctamente.');
+    } else {
+      // Aquí puedes agregar tu código adicional para el caso de una reserva exitosa
+      alert('¡Reserva exitosa!');
+    }
+  });
+  
--- main2.js
+++ main2.js
@@ -1,17 +1,16 @@
-document.getElementById('reservationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var email = document.getElementById('email').value;
-    var fecha = document.getElementById('fecha').value;
-    var comensales = document.getElementById('comensales').value;
-  
-    if (nombre.trim() === '' || apellido.trim() === '' || email.trim() === '' || fecha.trim() === '' || comensales.trim() === '' || isNaN(comensales)) {
-      alert('Por favor, complete todos los campos correctamente.');
-    } else {
-      
-      alert('¡Reserva exitosa!');
-      document.getElementById('reservationForm').reset();
-    }
-  });
+document.getElementById('subscriptionForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var nombre = document.getElementById('nombre').value;
+  var apellido = document.getElementById('apellido').value;
+  var correo = document.getElementById('correo').value;
+  var usuario = document.getElementById('usuario').value;
+  var password = document.getElementById('password').value;
+
+  if (nombre.trim() === '' || apellido.trim() === '' || correo.trim() === '' || usuario.trim() === '' || password.trim() === '') {
+    alert('Falta completar algún campo');
+  } else {
+    alert('¡Usted está suscripto!');
+    document.getElementById('subscriptionForm').reset();
+  }
+});
