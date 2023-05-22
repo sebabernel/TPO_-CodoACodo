3dc9a01df1766e8931678ec079d4485d123291b9
Sebas <sebastianbernel@gmail.com>
2023-05-21 22:18:48 -0300

    Actualizacion de pagina

--- js/main.js
+++ js/main.js
@@ -2,12 +2,5 @@ function showMenu(){
     document.querySelector('.navigation').classList.toggle('active');
     document.querySelector('.menu').classList.toggle('hide');
     document.querySelector('.close').classList.toggle('show');
-}
-
-function mostrarPopUp() {
-    // Mostrar el pop-up
-    document.getElementById("pop-up").style.display = "block";
-  
-    // Evitar que se envíe el formulario de inmediato
-    return false;
-  }
+ }
+ 
---
+++ js/main3.js
@@ -0,0 +1,15 @@
+ 
+ document.getElementById('reservationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var fecha = document.getElementById('fecha').value;
+    var comensales = document.getElementById('comensales').value;
+     if (nombre.trim() === '' || apellido.trim() === '' || email.trim() === '' || fecha.trim() === '' || comensales.trim() === '' || isNaN(comensales)) {
+      alert('Por favor, complete todos los campos correctamente.');
+    } else {
+    
+      alert('¡Reserva exitosa!');
+    }
+  });
